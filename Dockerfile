b2b343c8c948da25858a7e8d4877adf35497d9ec
Joel Hedlund <joel@nsc.liu.se>
2018-01-27 12:04:10 +0100

    No need to pull git repo in build process.

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
 FROM ubuntu:artful
 
-RUN apt-get update && apt-get install -y git wget build-essential locales
-RUN git clone https://github.com/neicnordic/psychflow.git
-
-COPY cache/* /psychflow/
+WORKDIR psychflow
+COPY setup*.sh vars.sh cache/* /psychflow/
 
 ENTRYPOINT /bin/bash
 
-WORKDIR psychflow
 RUN bash -e setup.sh
 
 # System specific setup
-RUN bash setup-mosler.sh
+RUN bash -e setup-mosler.sh
 
 WORKDIR /
 RUN rm -rf psychflow
--- setup.sh
+++ setup.sh
@@ -2,17 +2,10 @@
 
 . vars.sh
 
-getfile() {
-  ls "$(basename $1)" || wget $1
-}
-
 ## Apt
-apt update
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && locale-gen en_US.utf8 && /usr/sbin/update-locale LANG=en_US.UTF-8
-
-getfile $shapeit_url
-tar xf $(basename $shapeit_url)
-chmod 755 $shapeit_name
+apt-get update
+apt-get install -y wget locales
+echo -e "en_US.UTF-8 UTF-8\nsv_SE.UTF-8 UTF-8" >> /etc/locale.gen && locale-gen en_US.utf8 && /usr/sbin/update-locale LANG=en_US.UTF-8
 
 ## CRAN R
 apt-get install -y dirmngr
@@ -23,14 +16,14 @@ apt-get install -y r-base
 #R --slave -e 'install.packages("rmarkdown", repos="https://ftp.acc.umu.se/mirror/CRAN/")'
 
 ## Rstudio
-getfile $rstudio_url
+wget -nc $rstudio_url
 yes | apt install libxslt1-dev ./$(basename $rstudio_url)
 ### FIXME
 #echo 'export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb' >>$SINGULARITY_ENVIRONMENT
 
 ## Ricopili
 apt-get -y install texlive
-getfile $ricopili_url
+wget -nc $ricopili_url
 tar xf $(basename $ricopili_url)
 mv rp_bin /opt/
 chmod 755 /opt/rp_bin
@@ -42,26 +35,26 @@ cd /opt/rp_bin
 cd -
 
 ## liftOver
-getfile $liftover_url
+wget -nc $liftover_url
 chmod 755 liftOver
 mv liftOver /usr/bin/liftOver
 
 ## Metal
-getfile $metal_url
+wget -nc $metal_url
 tar xf $(basename $metal_url)
 mv generic-metal /opt/metal
 chmod 755 /opt/metal
 ln -s /opt/metal/metal /usr/bin/
 
 ## SHAPEIT
-getfile $shapeit_url
+wget -nc $shapeit_url
 tar xf $(basename $shapeit_url)
 chmod 755 $shapeit_name
 mv $shapeit_name /opt
 ln -s /opt/$shapeit_name/bin/shapeit /usr/bin/
 
 ## IMPUTE2
-getfile $impute_url
+wget -nc $impute_url
 tar xf $(basename $impute_url)
 impute_name=$(basename $impute_url .tgz)
 chmod 755 $impute_name
@@ -70,7 +63,7 @@ ln -s /opt/$impute_name/impute2 /usr/bin/
 
 ## PLINK
 apt-get install -y unzip
-getfile $plink_url
+wget -nc $plink_url
 unzip -d /opt/plink/ $(basename $plink_url)
 ln -s /opt/plink/plink /usr/bin/
 ln -s /opt/plink/prettify /usr/bin/
@@ -78,27 +71,24 @@ ln -s /opt/plink/prettify /usr/bin/
 ## EIGENSOFT
 eigensoft_version=$(basename $eigensoft_url .tar.gz)
 eigensoft_version=${eigensoft_version#v}
-apt-get install -y libopenblas-dev liblapacke-dev libgsl-dev
-getfile $eigensoft_url
+apt-get install -y build-essential libopenblas-dev liblapacke-dev libgsl-dev
+wget -nc $eigensoft_url
 tar xf $(basename $eigensoft_url)
 cd "EIG-$eigensoft_version/src"
 make clobber
 make install LDLIBS="-llapacke"
 cd -
-mv "EIG-$version" /opt/
+mv "EIG-$eigensoft_version" /opt/
 ### FIXME
 #echo 'export PATH=${PATH:+$PATH:}/opt/'"EIG-$version"'/bin' >>$SINGULARITY_ENVIRONMENT
 
 ## EAGLE
-getfile $eagle_url
+wget -nc $eagle_url
 tar xf $(basename $eagle_url)
 eagle_name=$(basename $eagle_url .tar.gz)
 chmod 755 $eagle_name
 mv $eagle_name /opt/
 ln -s /opt/$eagle_name/eagle /usr/bin/
-
-## System specific setup
-. setup-mosler.sh
 
 ## Good practice to update library path
 ldconfig
